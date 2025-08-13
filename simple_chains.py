8f8f013899b1bc4be58d79430933451b1038e287
Waqar Ali <waqar0343930@gmail.com>
2025-08-13 11:20:14 +0500

    simple chains

---
+++ simple_chains.py
@@ -0,0 +1,23 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template="What is the capital of {input}?",
+    input_variables=["input"]
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({"input": "France"})
+
+print(result)  # Output: Paris
+print(type(result))  # Output: <class 'str'>
+
